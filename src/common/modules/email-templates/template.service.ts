bdc2dda3ced71eac72492c1dc16af33e9b80bc1b
kngan99git <ngan.le.bku@hcmut.edu.vn>
2021-04-27 16:01:45 +0700

    debug

--- src/common/modules/email-templates/template.service.ts
+++ src/common/modules/email-templates/template.service.ts
@@ -17,7 +17,8 @@ export class TemplatesService {
     nickname?: string,
     role?: TOKEN_ROLE,
   ): any {
-    const pathDir = path.join(__dirname, '../../../templates');
+    const pathDir = path.join(__dirname, '../../../../templates');
+    console.log(pathDir);
     if (!fs.existsSync(`${pathDir}\\${template}`)) {
       customThrowError('Resource not found!', HttpStatus.NOT_FOUND);
     }
