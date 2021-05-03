3a16fe200c7647cc1143fb776524a243a723b038
tustran9499 <minhtu.tran.09041999@gmail.com>
2021-05-03 15:26:09 +0700

    [f] - Add customer select

--- src/modules/customers/customers.controller.ts
+++ src/modules/customers/customers.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiTags, ApiOkResponse } from '@nestjs/swagger';
+import { CustomersService } from './customers.service';
+import { Customer } from 'src/entities/customer/customer.entity';
 
+@ApiTags('Customer')
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+    constructor(private CustomersService: CustomersService) { }
+    @Get('/search')
+    @ApiOkResponse()
+    getAllCustomers(
+        @Query('key') key: string = "",
+    ): Promise<Customer[]> {
+        return this.CustomersService.search(key);
+    }
+}
--- src/modules/customers/customers.service.ts
+++ src/modules/customers/customers.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Customer } from 'src/entities/customer/customer.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CustomersService {}
+export class CustomersService {
+    constructor(
+        @InjectRepository(Customer)
+        private customersRepository: Repository<Customer>,
+    ) { }
+
+    async search(key: string): Promise<Customer[]> {
+        return this.customersRepository.createQueryBuilder('customers').where('customers.ContactName Like \'%' + String(key) + '%\'').orWhere('customers.Phone Like \'%' + String(key) + '%\'').getMany();
+    }
+}
