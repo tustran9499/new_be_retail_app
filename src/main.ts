ba62a614de6a27336073e7f989b28ad57823bbf4
Le Thi Kim Ngan <ngan.le@netpower.no>
2021-05-13 12:05:08 +0700

    add bearer option

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
     .setTitle('Retail System API')
     .setDescription('Retail system API')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
