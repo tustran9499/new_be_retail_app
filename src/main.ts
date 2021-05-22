c2db19d2d8f49351bf0e0ad4885adf34dad72848
tustran9499 <minhtu.tran.09041999@gmail.com>
2021-05-22 16:20:06 +0700

    [f] - Fix payload error

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { urlencoded, json } from 'body-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
@@ -23,7 +24,15 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+  app.use(json({
+    limit: '50mb'
+  }));
 
+  app.use(urlencoded({
+    limit: '50mb',
+    parameterLimit: 100000,
+    extended: true
+  }));
   await app.listen(process.env.PORT || 4000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
